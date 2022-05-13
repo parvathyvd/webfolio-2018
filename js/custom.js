c08a3b61e6e1ff847b4bfea8a1261add0ff0027f
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2022-05-12 19:00:24 -0700

    changed bg video to bg-image for better loading and some styling changes

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
-
-function toggleLogo (){
-
-  let logo = document.getElementById("footer-logo").src = "img/logo-white.png";
-
+function toggleLogo() {
+  let logo = (document.getElementById("footer-logo").src =
+    "img/logo-white.png");
 }
+
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
